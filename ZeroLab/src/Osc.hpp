24e80e9d6e973374869ec8d6497a41adc20df1f2
Daniel Billotte <daniel@litfuze.com>
2021-08-25 01:50:08 -0700

    finished phatty and worked on class/ctor structure some

--- ZeroLab/src/Osc.hpp
+++ ZeroLab/src/Osc.hpp
@@ -1,17 +1,40 @@
+/**
+ * @file Osc.hpp
+ * @author Daniel Billotte
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include "AudioLib.hpp"
 
 
-//--------------------------------------------------------
-class OscBase {
-public:
-    float _phase = 0.f;
-    
+typedef std::function<float(float)> PhaseToSample;
+
+
+/**
+ * @brief Base class for oscillators that operate off of a phase cycle of 2π.
+ * 
+ * OscBase is meant to be a starting point that has the core plumbing taken care
+ * of so that different waveforms or waveform generation methods can be
+ * explored.
+ */
+class OscBase {    
 protected:
+    int _sampleRate = 0;
     float _freq = 0.f;
     float _phaseInc = 0.f;
-    int _sampleRate = 0;
+    float _phase = 0.f;
     
 public:
+    /**
+     * @brief Set the oscillator frequency and recalculate the phase increment
+     * 
+     * @param freq : frequency in Hz
+     */
     void setFreq(float freq) {
         if(_freq != freq) {
             _freq = freq;
@@ -19,6 +42,11 @@ public:
         }
     }
     
+    /**
+     * @brief Set the sample-rate and recalculate the phase increment
+     * 
+     * @param sampleRate : sample-rate in samples/second
+     */
     void setSampleRate(int sampleRate) {
         if(_sampleRate != sampleRate) {
             _sampleRate = sampleRate;
@@ -26,6 +54,12 @@ public:
         }
     }
 
+    /**
+     * @brief Set the frequency and sample-rate, then recalculate the phase increment
+     * 
+     * @param freq : frequency in Hz
+     * @param sampleRate : sample-rate in samples/second
+     */
     void setPhaseParams(float freq, int sampleRate) {
         _freq = freq;
         _sampleRate = sampleRate;
@@ -68,23 +102,98 @@ protected:
     }
 };
 
+class SingleOsc2 : public SingleOsc {
+public:
+    SingleOsc2() : _generator { _IDENTITY_F } {}
+    SingleOsc2(PhaseToSample &generator) : _generator { generator } {}
+
+    void setGenerator(PhaseToSample &generator) {
+        _generator = generator;
+    }
+
+    float _getValue() {
+        return _generator(_phase);
+    }
+
+protected:
+    PhaseToSample& _generator;
+    PhaseToSample _IDENTITY_F = [](float phase){ return phase; };
+};
+
+
+//--------------------------------------------------------
+// With SingleOsc2 subclasses can be very simple
+PhaseToSample sinGenerator = [](float _phase) { return sin(_phase); };
+class SineOsc2 : public SingleOsc2 {
+    SineOsc2() : SingleOsc2{sinGenerator} {}
+};
+
+//--------------------------------------------------------
+class TriangleOsc : public SingleOsc {
+protected:
+    float _getValue() override {
+        float tri_out = _phase * 2.f / _PI;
+        
+        if(_phase < _PI/2.f) {
+            //tri_out = 0.f + tri_out;
+        } else if(_phase < _3halfPI) {
+            tri_out = 2.f - tri_out;
+        } else {
+            tri_out = -4.f + tri_out;
+        }
+        
+        return tri_out;
+    }
+};
+
+PhaseToSample triangleGenerator = [](float phase) {
+    float tri_out = phase * 2.f / _PI;
+
+    if(phase < _PI/2.f) {
+        return tri_out;
+    } else if(phase < _3halfPI) {
+        return 2.f - tri_out;
+    } else {
+        return -4.f + tri_out;
+    }
+};
+
+class TriangleOsc2 : public SingleOsc2 {
+    TriangleOsc2() : SingleOsc2{ triangleGenerator } {};
+};
+
+//--------------------------------------------------------
+class SawOsc : public SingleOsc {
+protected:
+    float _getValue() override {
+        return _phase/_PI - 1.f;
+    }
+};
+
+//--------------------------------------------------------
+class SquareOsc : public SingleOsc {
+protected:
+    float _getValue() override {
+        return (_phase < _PI) ? -1.f : 1.f;
+    }
+};
+
 
 //--------------------------------------------------------
 
 
-typedef std::function<float(float)> F2F;
 
 class DIYQuadrant : public SingleOsc {
 public:
-    void setQ1(F2F f) { _q1 = f; }
-    void setQ2(F2F f) { _q2 = f; }
-    void setQ3(F2F f) { _q3 = f; }
-    void setQ4(F2F f) { _q4 = f; }
+    void setQ1(PhaseToSample f) { _q1 = f; }
+    void setQ2(PhaseToSample f) { _q2 = f; }
+    void setQ3(PhaseToSample f) { _q3 = f; }
+    void setQ4(PhaseToSample f) { _q4 = f; }
     
-    void setAllQ(F2F f1,
-                 F2F f2,
-                 F2F f3,
-                 F2F f4) {
+    void setAllQ(PhaseToSample f1,
+                 PhaseToSample f2,
+                 PhaseToSample f3,
+                 PhaseToSample f4) {
         _q1 = f1;
         _q2 = f2;
         _q3 = f3;
@@ -106,11 +215,52 @@ protected:
         return clamp(out, -1.f, 1.f);
     }
     
-    F2F _q1;
-    F2F _q2;
-    F2F _q3;
-    F2F _q4;
+    PhaseToSample _q1;
+    PhaseToSample _q2;
+    PhaseToSample _q3;
+    PhaseToSample _q4;
 };
+//--------------------------------------------------------
+class KindaEvenOsc : public DIYQuadrant {
+public:
+    KindaEvenOsc() {
+        setAllQ([](float _phase){ return sin(_phase); },
+                [](float _phase){ return (-sin(_phase)*0.5f) + 0.2f; },
+                [](float _phase){ return (-sin(_phase)*0.5f) - 0.2f; },
+                [](float _phase){ return sin(_phase); }
+                );
+    }
+};
+
+//--------------------------------------------------------
+// diff implementation of above. instead of using the
+// diyquadrant,
+PhaseToSample quadGen(PhaseToSample& q1,
+                    PhaseToSample& q2,
+                    PhaseToSample& q3,
+                    PhaseToSample& q4) {
+    return [q1, q2, q3, q4](float phase) {
+        float out = 0.f;
+        if(phase < _PI/2.f)
+            out = q1(phase);
+        else if(phase < _PI)
+            out = q2(phase);
+        else if(phase < _3halfPI)
+            out = q3(phase);
+        else
+            out = q4(phase);
+        return out;
+    };
+};
+
+// PhaseToSample keGen = quadGen([](float _phase){ return sin(_phase); },
+//                 [](float _phase){ return (-sin(_phase)*0.5f) + 0.2f; },
+//                 [](float _phase){ return (-sin(_phase)*0.5f) - 0.2f; },
+//                 [](float _phase){ return sin(_phase); }
+//                 );
+// class KindaEven2 : public SingleOsc2 {
+//     KindaEven2() : SingleOsc2{ keGen } {}
+// };
 
 //--------------------------------------------------------
 template<class T>
@@ -134,6 +284,12 @@ enum SineSawVoices {
 
 class SineSawOsc : public MultipleVoiceOsc<SineSawVoices> {
 protected:
+    float _sin_out = 0.f;
+
+    void _initCycle() override {
+        _sin_out = sin(_phase);
+    }
+
     float _getValue(SineSawVoices type) override {
         if(type == VOICE_SINE)
             return _sin_out;
@@ -141,13 +297,110 @@ protected:
         // type == VOICE_SAW
         return _phase/_PI - 1.f;
     }
-    
-protected:
-    float _sin_out=0;
+};
+
+//--------------------------------------------------------
+enum PhattyVoices {
+    VOICE_SINE1,
+    VOICE_SAW1,
+    VOICE_SQR1,
+    VOICE_SINE_SUB1,
+    VOICE_SQR_SUB1,
+    VOICE_WHA_SUB1,
+    VOICE_SINE_SUB2,
+    VOICE_SQR_SUB2,
+    VOICE_WHA_SUB2,
+    NUM_VOICE
+};
+
+
+class Phatty : public MultipleVoiceOsc<PhattyVoices> {
+    int _sub_cycle_count = 0;
+    float _last_phase = 0.f;
+    float _phase_sub1 = 0.f;
+    float _phase_sub1x = 0.f;
+    float _sine_sub1 = 0.f;
+    float _phase_sub2 = 0.f;
+    float _phase_sub2x = 0.f;
+    float _sine_sub2 = 0.f;
+
     void _initCycle() override {
-        _sin_out = sin(_phase);
+        if(_last_phase > _phase)
+            if(_sub_cycle_count++ >= 3)
+                _sub_cycle_count = 0;
+        _last_phase = _phase;
+
+        // this was a bug that produced a cool sound
+        _phase_sub1x = (_phase + _sub_cycle_count) * _PI;
+        _phase_sub2x= (_phase + _sub_cycle_count) * _halfPI;
+        
+        // this one is cool too
+        // _phase_sub1 = _phase + (_sub_cycle_count * _PI);
+        // _phase_sub2 = _phase + (_sub_cycle_count * _halfPI);
+    
+        // this one is correct
+        _phase_sub1 = (_phase + ((_sub_cycle_count % 2) * _twoPI))/2.f;
+        _phase_sub2 = (_phase + (_sub_cycle_count * _twoPI))/4.f;
+
+    }
+
+    float _getValue(PhattyVoices type) override {
+        if(type == VOICE_SINE1) {
+            return sin(_phase);
+        } else if(type == VOICE_SAW1) {
+            return _phase/_PI - 1.f;
+        } else if(type == VOICE_SQR1) {
+            return (_phase < _PI) ? -1.f : 1.f;
+        } else if(type == VOICE_SINE_SUB1) {
+            return  sin(_phase_sub1);
+        } else if(type == VOICE_SINE_SUB2) {
+            return sin(_phase_sub2);
+        } else if(type == VOICE_SQR_SUB1) {
+            return (_phase_sub1 < _PI) ? -1.f : 1.f;
+        } else if(type == VOICE_SQR_SUB2) {
+            return (_phase_sub2 < _PI) ? -1.f : 1.f;
+        } else if(type == VOICE_WHA_SUB1) {
+            return sin(_phase_sub1x);
+        } // else VOICE_WHA_SUB2
+        return sin(_phase_sub2x);
     }
 };
+
+// this was awesome as is, just in case I screw it up while working on it
+// class Phatty : public MultipleVo          siceOsc<PhattyVoices> {
+//     int _sub_cycle_count = -1;
+//     float _last_phase = 0.f;
+
+//     void _initCycle() {
+//         if(_last_phase > _phase)
+//             if(_sub_cycle_count++ > 1)
+//                 _sub_cycle_count = 0;
+//         _last_phase = _phase;
+//     }
+
+//     float _getValue(PhattyVoices type) override {
+//         if(type == VOICE_SINE1) {
+//             return sin(_phase);
+//         } else if(type == VOICE_SAW1) {
+//             return _phase/_PI - 1.f;
+//         } else if(type == VOICE_SQR1) {
+//             return (_phase < _PI) ? -1.f : 1.f;
+//         } //else if(type == VOICE_SINE_SUB1) {
+        
+//             if(_sub_cycle_count == 0) {
+//                 return sin(_phase/2.f);
+//             } else {
+//                 return -sin(_phase/2.f);
+//             }
+            
+
+//         //}
+//     }
+// };
+
+
+
+
 
 
 
--- ZeroLab/src/Osc1.cpp
+++ ZeroLab/src/Osc1.cpp
@@ -2,179 +2,105 @@
 //#include "AudioLib.hpp"
 #include "Osc.hpp"
 
-/*
- 
- For the Osc class have two different interfaces
- - one shot: get the next sample from the osc and advance it
-    float out = osc.nextSine();
- 
- - multiple: get whichever samples wanted, then advance it
-    float sine = osc.sine();
-    float sqr = osc.square();
-    osc.next();
- 
- */
-//class SineOsc {
-//protected:
-//    float _freq = 0.f;
-//    float _phaseInc = 0.f;
-//    float _phase = 0.f;
-//    int _sampleRate = 0;
-//
-//public:
-//    void setFreq(float freq) {
-//        if(_freq != freq) {
-//            _freq = freq;
-//            setPhaseInc();
-//        }
-//    }
-//
-//    void setSampleRate(int sampleRate) {
-//        if(_sampleRate != sampleRate) {
-//            _sampleRate = sampleRate;
-//            setPhaseInc();
-//        }
-//    }
-//
-//    void setPhaseParams(float freq, int sampleRate) {
-//        _freq = freq;
-//        _sampleRate = sampleRate;
-//        setPhaseInc();
-//    }
-//
-//    float next() {
-//        float out = sin(_phase);
-//
-//        _phase += _phaseInc;
-//        if(_phase > _twoPI)
-//            _phase -= _twoPI;
-//
-//        return out;
-//    }
-//
-//protected:
-//    void setPhaseInc() {
-//        _phaseInc = _twoPI * _freq / _sampleRate;
-//    }
-//};
-
-
 struct Osc1 : Module {
 	enum InputIds {
 		VOCT_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
-		RAMP_OUTPUT,
 		SIN_OUTPUT,
+		SAW_OUTPUT,
 		SQR_OUTPUT,
-		TRI_OUTPUT,
-        WHA_OUTPUT,
+		SINSUB1_OUTPUT,
+        SQRSUB1_OUTPUT,
+        SINSUB2_OUTPUT,
+        SQRSUB2_OUTPUT,
 		NUM_OUTPUTS
 	};
     enum ParamIds {
         FREQ_PARAM,
-        RANGE_PARAM,
-        POLARITY_PARAM,
+        // RANGE_PARAM,
+        // POLARITY_PARAM,
         NUM_PARAMS
     };
 	enum LightIds {
 		NUM_LIGHTS
 	};
-
+    
+    // SineSawOsc sinesaw = SineSawOsc();
+    // TriangleOsc triangle = TriangleOsc();
+    // DIYQuadrant quad = DIYQuadrant();
+    Phatty phatty = Phatty();
+    
 	Osc1() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(FREQ_PARAM, 0, 10000, 440, "Freq(Hz)");
-		configParam(RANGE_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(POLARITY_PARAM, 0.f, 1.f, 0.f, "");
+		// configParam(RANGE_PARAM, 0.f, 1.f, 0.f, "");
+		// configParam(POLARITY_PARAM, 0.f, 1.f, 0.f, "");
         
-        quad.setAllQ([](float phase){ return sin(phase); },
-                     [](float phase){ return -1.f + sin(phase); },
-                     [](float phase){ return -sin(phase); },
-                     [](float phase){ return 1.f - sin(phase); });
+        // quad.setAllQ([](float phase){ return sin(phase); },
+        //              [](float phase){ return -1.f + sin(phase); },
+        //              [](float phase){ return -sin(phase); },
+        //              [](float phase){ return 1.f - sin(phase); });
 	}
+    
+    // void onSampleRateChange() override {
+    //     sinesaw.setSampleRate()
+    // }
 
-    float _phase_inc = 0.f;
-    float _phase = 0.f;
-    float _freq = 0.f;
-    
-//    SineOsc sine = SineOsc();
-    SineSawOsc sinesaw = SineSawOsc();
-    DIYQuadrant quad = DIYQuadrant();
-    
-    
 	void process(const ProcessArgs& args) override {
-        float new_freq = params[FREQ_PARAM].getValue();
-        if(_phase_inc == 0 || (_freq != new_freq)) {
-            _freq = new_freq;
-            _phase_inc = _twoPI * _freq / args.sampleRate;
-        }
+        float freq = params[FREQ_PARAM].getValue();
         
-        // Sine Wave
-//        float sin_out = sin(_phase); // this is also used for the square wave
+        // quad.setPhaseParams(freq, args.sampleRate);
+        // sinesaw.setPhaseParams(freq, args.sampleRate);
+        // triangle.setPhaseParams(freq, args.sampleRate);
+        phatty.setPhaseParams(freq, args.sampleRate);
+        
+        outputs[SIN_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_SINE1));
+        outputs[SAW_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_SAW1));
+        outputs[SQR_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_SQR1));
+        outputs[SINSUB1_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_WHA_SUB1));
+        outputs[SQRSUB1_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_SQR_SUB1));
+        outputs[SINSUB2_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_WHA_SUB2));
+        outputs[SQRSUB2_OUTPUT].setVoltage(5 * phatty.getValue(VOICE_SQR_SUB2));
+        
+        phatty.next();
 
-        quad.setPhaseParams(new_freq, args.sampleRate);
-        sinesaw.setPhaseParams(new_freq, args.sampleRate);
+//         // Sine Wave
+//         float sin_out = sinesaw.getValue(VOICE_SINE);
+//         if(outputs[SIN_OUTPUT].isConnected()) {
+//             outputs[SIN_OUTPUT].setVoltage(5.f*phatty.getValue(VOICE_SINE1));
+//         }
         
-        float sin_out = sinesaw.getValue(VOICE_SINE);
-        
-        sin_out = quad.getValue();
-        
-        if(outputs[SIN_OUTPUT].isConnected()) {
-            outputs[SIN_OUTPUT].setVoltage(5.f*sin_out);
-        }
-
-        
-        // Triangle Wave
-        if(outputs[TRI_OUTPUT].isConnected()) {
-            float tri_out = _phase * 2.f / _PI;
-            
-            if(_phase < _PI/2.f) {
-                //tri_out = 0.f + tri_out;
-            } else if(_phase < _3halfPI) {
-                tri_out = 2.f - tri_out;
-            } else {
-                tri_out = -4.f + tri_out;
-            }
-            
-            outputs[TRI_OUTPUT].setVoltage(5.f*tri_out);
-            
-        }
+//         // Triangle Wave
+//         if(outputs[TRI_OUTPUT].isConnected()) {
+//             float tri_out = triangle.getValue();
+//             outputs[TRI_OUTPUT].setVoltage(5.f*tri_out);
+//             outputs[TRI_OUTPUT].setVoltage(5.f*phatty.getValue(VOICE_SINE_SUB2));
+//         }
                
-        // Saw Wave
-        if(outputs[RAMP_OUTPUT].isConnected()) {
-//            outputs[RAMP_OUTPUT].setVoltage(5.f*(_phase/_PI - 1.f));
-            outputs[RAMP_OUTPUT].setVoltage(5.f * sinesaw.getValue(VOICE_SAW));
-        }
-        sinesaw.next();
+//         // Saw Wave
+//         if(outputs[RAMP_OUTPUT].isConnected()) {
+// //            outputs[RAMP_OUTPUT].setVoltage(5.f*(_phase/_PI - 1.f));
+//             outputs[RAMP_OUTPUT].setVoltage(5.f * sinesaw.getValue(VOICE_SAW));
+//         }
         
-        // Square Wave
-        if(outputs[SQR_OUTPUT].isConnected()) {
-            float sqr_out = (sin_out > 0.f) ? 1.f : -1.f;
-            outputs[SQR_OUTPUT].setVoltage(5.f*sqr_out);
-        }
+//         // Square Wave
+//         if(outputs[SQR_OUTPUT].isConnected()) {
+//             // float sqr_out = (sin_out > 0.f) ? 1.f : -1.f;
+//             // outputs[SQR_OUTPUT].setVoltage(5.f*sqr_out);
+//             outputs[SQR_OUTPUT].setVoltage(5.f*phatty.getValue(VOICE_SQR1));
+//         }
         
-        // WHA th ...?
-        if(outputs[WHA_OUTPUT].isConnected()) {
-            float tri_out = _phase * 2.f / _PI;
-            
-            if(_phase < _PI/2.f) {
-                tri_out = sin_out;
-            } else if(_phase < _PI) {
-                tri_out = (-sin_out*0.5f) + 0.2f;
-            } else if(_phase < _3halfPI) {
-                tri_out = (-sin_out*0.5f) - 0.2f;
-            } else {
-                tri_out = sin_out;
-            }
-    
-            outputs[WHA_OUTPUT].setVoltage(5*tri_out);
-        }
-        
-        
-        _phase += _phase_inc;
-        if(_phase > _twoPI)
-            _phase -= _twoPI;
+//         // WHA th ...?
+//         if(outputs[WHA_OUTPUT].isConnected()) {
+//             // float out = quad.getValue();
+//             // outputs[WHA_OUTPUT].setVoltage(5*tri_out);
+//             outputs[WHA_OUTPUT].setVoltage(5.f*phatty.getValue(VOICE_SINE_SUB1));
+//         }
+
+//         sinesaw.next();
+//         phatty.next();
 	}
 };
 
@@ -190,19 +116,21 @@ struct Osc1Widget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.092, 21.907)), module, Osc1::FREQ_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(9.62, 74.753)), module, Osc1::RANGE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.033, 74.981)), module, Osc1::POLARITY_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(18.967, 48.537)), module, Osc1::VOCT_INPUT));
+		// addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(9.62, 74.753)), module, Osc1::RANGE_PARAM));
+		// addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.033, 74.981)), module, Osc1::POLARITY_PARAM));
+		// addInput(createInputCentered<PJ301MPort>(mm2px(Vec(18.967, 48.537)), module, Osc1::VOCT_INPUT));
 
-		
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.f, 104.f)), module, Osc1::SIN_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(31.f, 104.f)), module, Osc1::RAMP_OUTPUT));
-		
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.f, 111.5f)), module, Osc1::WHA_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.f, 90.f)), module, Osc1::SIN_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(31.f, 90.f)), module, Osc1::SQR_OUTPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.f, 119.f)), module, Osc1::TRI_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(31.f, 119.f)), module, Osc1::SQR_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.f, 110.f)), module, Osc1::SAW_OUTPUT));
+
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.f, 105.f)), module, Osc1::SINSUB1_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(31.f, 105.f)), module, Osc1::SQRSUB1_OUTPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.f, 120.f)), module, Osc1::SINSUB2_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(31.f, 120.f)), module, Osc1::SQRSUB2_OUTPUT));
     }
 };
 
