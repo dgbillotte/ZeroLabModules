e177a5f19e172916c1c41f7fc3b59f4195c780d7
Daniel Billotte <daniel@litfuze.com>
2021-10-05 13:02:31 -0700

    the Reverb class just kept crashing,

--- src/Reverb1.cpp
+++ src/Reverb1.cpp
@@ -1,12 +1,15 @@
 #include "plugin.hpp"
-#include "AllPassFilter.hpp"
-#include "CombFilter.hpp"
-#include "Reverb.hpp"
+// #include "AllPassFilter.hpp"
+// #include "CombFilter.hpp"
+#include "ReverbS.hpp"
 /*
  * This plugin is based off Will Pirkle's book ...
  */
 
-
+// this *need* to be zero-terminated :-D
+int _combFreqsEven[] = {1321, 1543, 1777, 1993, 0};
+int _combFreqsGR[] = {1321, 1447, 1657, 1993, 0};
+int _apfFreqs[] = {89, 163, 0};
 struct Reverb1 : Module {
 	enum ParamIds {
 		RT60_PARAM,
@@ -26,97 +29,26 @@ struct Reverb1 : Module {
 		NUM_LIGHTS
 	};
 	
-	/*
-	 * I calculated the delays based on Schroeder's suggestion
-	 * of 1:1.5 ratio for the delays of the comb filters, specifically
-	 * 30-45msec. I calc'd this based on 44100 and then split the range
-	 * up two ways. First was evenly, but I rounded to the nearest prime:
-	 * D0: 1321, 1543, 1777, 1993
-	 * The second is based on the golden ratio, again rounding to nearest prime
-	 * D1: 1321, 1447, 1657, 1993
-	 */
-	int cf_delays[2][4] = {
-		{1321, 1447, 1657, 1993},
-		{1321, 1543, 1777, 1993}
-	};
-	int cf_delay_idx = 0;
-	const size_t num_combs = 4;
-	CombFilter* combs[4];
-
-	/*
-	 * I calculated these, again based on Schroeder's suggestion, 
-	 * of 1-5msec. This is the list of primes to choose from:
-	 * [43, 47, 53, 61, 71, 73, 79, 83, *89, 97, 101, 103, 107, 109, 113,
-	 *  127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
-	 *  193, 197, 199, 211, 223, 227, 229, *233]
-	 */
-
-	int apf_delays[4][2] = {
-		{43,47},
-		{43,233},
-		{89,149},
-		{89,233}
-	};
-	int apf_delay_idx = 3;
-	const size_t num_apfs = 2;
-	AllPassFilter* apfs[2];
+	ReverbS reverb = ReverbS(); 
 
 	Reverb1() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(RT60_PARAM, 0.f, 10.f, 0.f, "RT60 (secs)");
 		configParam(APG_PARAM, 0.f, 0.99f, 0.5f, "g for the all-pass stage");
 		configParam(MIX_PARAM, 0.f, 1.f, 0.f, "Wet/Dry Mix");
-
-		initFilters(cf_delays[cf_delay_idx], apf_delays[apf_delay_idx], 0.5f);
 	}
 
-	void initFilters(int* comb_freqs, int* apf_freqs, float g) {
-		float rt60 = 1.f;
-		int sample_rate = 44100;
-		for(size_t i=0; i < num_combs; i++) {
-			int freq = comb_freqs[i];
-			combs[i] = new CombFilter(freq);
-			combs[i]->g(calcCFG(freq, rt60, sample_rate));
-		}
-
-		for(size_t i=0; i < num_apfs; i++) {
-			int freq = apf_freqs[i];
-			apfs[i] = new AllPassFilter(freq);
-			apfs[i]->g(g);
-		}
-	}
-
-	const float wet_gain = 0.75f; //08f;
 	void process(const ProcessArgs& args) override {
 		float input = inputs[AUDIO_INPUT].getVoltage();
 		float rt60 = params[RT60_PARAM].getValue();
 		float apg = params[APG_PARAM].getValue();
-
-		cookParams(rt60, apg, args.sampleRate);
-
-		float comb_step = combs[0]->process(input) + combs[1]->process(input) +
-						combs[2]->process(input) + combs[3]->process(input);
-		float wet_out = wet_gain * apfs[1]->process(apfs[0]->process(comb_step));
-
 		float mix = params[MIX_PARAM].getValue();
-		float out = ((1-mix) * input) + (mix * wet_out); // this should prob use log scale
 
-		outputs[AUDIO_OUTPUT].setVoltage(out);
-	}
+		reverb.rt60(rt60);
+		reverb.apfG(apg);
+		reverb.dryWetMix(mix);
 
-	void cookParams(float rt60, float apg, int sample_rate) {
-
-		combs[0]->g(calcCFG(cf_delays[cf_delay_idx][0], rt60, sample_rate));
-		combs[1]->g(calcCFG(cf_delays[cf_delay_idx][1], rt60, sample_rate));
-		combs[2]->g(calcCFG(cf_delays[cf_delay_idx][2], rt60, sample_rate));
-		combs[3]->g(calcCFG(cf_delays[cf_delay_idx][3], rt60, sample_rate));
-
-		apfs[0]->g(apg);
-		apfs[1]->g(apg);
-	}
-
-	float calcCFG(size_t delay, float rt60, int sample_rate) {
-		return pow(10.f, (-3.f*delay/sample_rate)/rt60);
+		outputs[AUDIO_OUTPUT].setVoltage(reverb.process(input));
 	}
 };
 
---
+++ src/ReverbS.hpp
@@ -0,0 +1,126 @@
+/*
+Schroeder Reverb:
+This is an implementation of the Schroeder Reverb as described by Will Pirkle.
+
+NOTE: Currently, when you reopen a patch with this module in it, the module
+exhibits not-very predictable behavior on start up and commonly locks into
+a super-saturated state. It will usually calm down if you unplug the input,
+turn all the knobs to zer0, and give it a second for the delays to
+clear out. I might get this fixed...
+
+It is composed of M parallel comb filters that are summed together and then
+passed through N all-pass filters serially. In the book, M=4 and N=2 but this
+implementation allows the user to determine M & N. Below is a 4x2 instance.
+
+The output gain on the above combo is pretty high (10x in some cases) so I've
+played with various means of taming it. Currently I just attenuate it by _wetGain
+right before mixing it to out.
+
+int combf_freqs[] =  {1321, 1447, 1657, 1993};
+int apf_freqs[] = {33, 89};
+ReverbS reverb = ReverbS(combf_freqs, apf_freqs);
+
+The comb filter delays are based on Schroeder's suggestion
+of 1:1.5 ratio for the delays of the comb filters, specifically
+30-45msec. I calc'd this based on 44100 and then split the range
+up two ways. First was evenly, but I rounded to the nearest prime:
+D0: 1321, 1543, 1777, 1993
+The second is based on the golden ratio, again rounding to nearest prime
+D1: 1321, 1447, 1657, 1993
+
+
+For the all-pass filter, again based on Schroeder's suggestion, 
+of 1-5msec. This is the list of primes to choose from:
+[43, 47, 53, 61, 71, 73, 79, 83, *89, 97, 101, 103, 107, 109, 113,
+ 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+ 193, 197, 199, 211, 223, 227, 229, *233]
+
+Some things to do...
+- currently the delays are in samples. Change to or add time based (sec or ms) spec.
+- figure out the startup lockup
+- make it possible to externally configure the delay times for
+  the filters and possibly the number of each
+- do log mixing instead of linear mixing?
+
+*/
+#ifndef REVERB
+#define REVERB
+
+#include "AllPassFilter.hpp"
+#include "CombFilter.hpp"
+
+class ReverbS {
+    CombFilter cf1 = CombFilter(1321, combG(1321));
+    CombFilter cf2 = CombFilter(1543, combG(1543));
+    CombFilter cf3 = CombFilter(1777, combG(1777));
+    CombFilter cf4 = CombFilter(1993, combG(1993));
+
+    AllPassFilter apf1 = AllPassFilter(33, 0.5f);
+    AllPassFilter apf2 = AllPassFilter(89, 0.5f);
+
+    float _rt60 = 0.5f;
+    float _apfG = 0.5f;
+    int _sampleRate = 44100;
+    float _dryWetMix = 0.5f;
+    int _dirty = false;
+    const float _wetGain = 0.7f;
+
+
+
+public:
+    // ReverbS(int sampleRate=44100, float rt60=0.5f, float apfG=0.5f, dryWetMix=0.5f) :
+    //     _rt60(rt60), _apfG(apfG), _sampleRate(sampleRate), _dryWetMix(dryWetMix) {}
+
+
+    void rt60(float rt60) {
+        if(_rt60 == rt60)
+            return;
+        _rt60 = rt60;
+        _dirty = true;
+    }
+
+    void apfG(float g) {
+        if(_apfG == g)
+            return;
+        _apfG = g;
+        _dirty = true;
+    }
+
+    void dryWetMix(float mix) {
+        _dryWetMix = mix;
+    }
+
+    float process(float input) {
+        if(_dirty) {
+            cf1.g(combG(cf1.delay()));
+            cf2.g(combG(cf2.delay()));
+            cf3.g(combG(cf3.delay()));
+            cf4.g(combG(cf4.delay()));
+
+            apf1.g(_apfG);
+            apf2.g(_apfG);
+
+            _dirty = false;
+        }
+
+        // run the comb filters
+        float out = cf1.process(input) + cf2.process(input) +
+            cf3.process(input) + cf4.process(input);
+
+        // run the all-pass filters            
+        float wetOut = _wetGain * apf2.process(apf1.process(out));
+
+        // mix it up, this should prob be log instead of linear
+        out = ((1-_dryWetMix) * input) + (_dryWetMix * wetOut);
+
+        return out;
+    }
+
+private:
+	float combG(size_t delay) {
+		return pow(10.f, (-3.f*delay/_sampleRate)/_rt60);
+	}
+};
+
+
+#endif
