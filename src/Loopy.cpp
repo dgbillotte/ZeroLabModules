0f79f565adbc13ecc50c2d8300aa65d44f38aeba
Daniel Billotte <daniel@litfuze.com>
2021-11-09 15:02:26 -0700

    added parameters for loop start and length

--- src/Loopy.cpp
+++ src/Loopy.cpp
@@ -3,10 +3,16 @@
 #include "lib/Components.hpp"
 #include "lib/DelayBuffer.hpp"
 
-
+/*
+ * VCV Things to figure out:
+ * - how to set a knob
+ * - better understanding of components: how to build them
+ */
 struct Loopy : Module {
 	enum ParamIds {
 		PHASE_INC_PARAM,
+		LOOP_START_PARAM,
+		LOOP_LENGTH_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -27,9 +33,17 @@ struct Loopy : Module {
 
 	bool _recording = false;
 	size_t _loopLength = 0;
-	size_t _playIndex = 0;
 	float _playFIdx = 0.f;
+	int _startIndex = 0;
+	bool _startIndexChanged = false;
+
+	// parameters
 	float _phaseIncr = 1.f;
+	int _loopStartOffset = 0.f;
+	float _loopPlayPct = 1.f;
+	int _loopPlayLength = 0;
+	
+
 
 	// dsp::SchmittTrigger _pluckTrig;
 	// dsp::SchmittTrigger _refretTrig;
@@ -37,6 +51,9 @@ struct Loopy : Module {
 	Loopy() : MAX_DELAY(4410), _loopBuffer(MAX_DELAY)	{
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(PHASE_INC_PARAM, -1.f, 1.f, 0.f, "Phase Increment Offset");
+		configParam(LOOP_START_PARAM, 0.f, 1000.f, 0.f, "Loop Start Offset in samples");
+		// configParam(LOOP_LENGTH_PARAM, -1.f, 1.f, 1.f, "Loop Length as a %");
+		configParam(LOOP_LENGTH_PARAM, 0.f, 1.f, 1.f, "Loop Length as a %");
 	}
 
 	void onSampleRateChange() override;
@@ -44,6 +61,7 @@ struct Loopy : Module {
 	int downsampleCount = 0;
 	int downsampleRate = 16;
 	void process(const ProcessArgs& args) override;
+	float _processSample(float input, bool record);
 	float _nextSample();
 
 };
@@ -62,12 +80,30 @@ void Loopy::process(const ProcessArgs& args) {
 
 		float phaseIncOffset = params[PHASE_INC_PARAM].getValue();
 		_phaseIncr = 1.f + phaseIncOffset;
+		
+		float loopPlayPct = params[LOOP_LENGTH_PARAM].getValue();
+		if(loopPlayPct != _loopPlayPct) {
+			_loopPlayPct = loopPlayPct;
+			_loopPlayLength = _loopLength * _loopPlayPct;
+
+		}
+
+		float startIndex = params[LOOP_START_PARAM].getValue();
+		if(_startIndex != startIndex) {
+			_startIndex = startIndex;
+			_startIndexChanged = true;
+		}
 	}
 
 	float audioIn = inputs[AUDIO_INPUT].getVoltage();
-	float sampleOut = 0.f;
-	float recordGate = inputs[RECORD_GATE_INPUT].getVoltage();
-	if(recordGate > 3.f) { // I use +3V so that  +0/5V or +/-5V binary signals will work as well as +0/10V
+	bool recordGate = inputs[RECORD_GATE_INPUT].getVoltage() > 3.f; // +3V works for all signal ranges we see
+	float audioOut = _processSample(audioIn, recordGate);
+	outputs[AUDIO_OUTPUT].setVoltage(audioOut);
+}
+
+inline float Loopy::_processSample(float audioIn, bool record) {
+
+	if(record) { 
 		// start recording
 		if(! _recording) {
 			_recording = true;
@@ -78,23 +114,23 @@ void Loopy::process(const ProcessArgs& args) {
 		// continue recording and play the current input
 		_loopBuffer.push(audioIn);
 		_loopLength++;
-		sampleOut = audioIn;
-		
-	} else {
-		// stop recording
-		if(_recording) {
-			_recording = false;
-			if(_loopLength > MAX_DELAY) {
-				_loopLength = MAX_DELAY;
-			}
-			_playIndex = 0;
-		}
 
-		// continue playing the current loop
-		sampleOut = _nextSample();
+		// return current sample in as the sample out
+		return audioIn;	
+	}
+	 
+	// stop recording
+	if(_recording) {
+		_recording = false;
+		if(_loopLength > MAX_DELAY) {
+			_loopLength = MAX_DELAY;
+		}
+		_playFIdx = _startIndex;
+		_loopPlayLength = _loopLength * _loopPlayPct;
 	}
 
-	outputs[AUDIO_OUTPUT].setVoltage(sampleOut);
+	// return next sample from the loop
+	return _nextSample();
 }
 
 inline float Loopy::_nextSample() {
@@ -104,31 +140,27 @@ inline float Loopy::_nextSample() {
 
 	int x0 = (int)_playFIdx;
 	float y0 = _loopBuffer.aRead(x0);
+		
+	// this could be a float, would it make an audible difference?
+	int stop = _loopLength * _loopPlayPct;
 
-	int x1 = (x0+1 < _loopLength) ? x0+1 : 0;
+	int x1 = (x0+1 < stop) ? x0+1 : 0;
 	float y1 = _loopBuffer.aRead(x1);
 
 	float sampleOut = y0 + (_playFIdx - x0) * (y1 - y0);
 
 	_playFIdx += _phaseIncr;
-	if(_playFIdx >= _loopLength) {
-		_playFIdx -= _loopLength;
+	if(_playFIdx >= stop) {
+		if(_startIndexChanged) {
+			_playFIdx = _startIndex;
+			_startIndexChanged = false;
+		} else {
+			_playFIdx -= stop;
+		}
 	}
 
 	return sampleOut;
 }
-// inline float Loopy::_nextSample() {
-// 	if(_loopLength == 0) {
-// 		return 0.f;
-// 	}
-
-// 	sampleOut = _loopBuffer.aRead(_playIndex);
-// 	if(++_playIndex == _loopLength) {
-// 		_playIndex = 0;
-// 	}
-// 	return sampleOut
-// }
-
 
 
 
@@ -164,9 +196,9 @@ struct LoopyWidget : ModuleWidget {
 		// addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col2, rowY)), module, Loopy::DECAY_PARAM));
 		// addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col3, rowY)), module, Loopy::STRETCH_PARAM));
 
-		// rowY += rowInc;
-		// addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col1, rowY)), module, Loopy::ATTACK_PARAM));
-		// addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col2, rowY)), module, Loopy::PICK_POS_PARAM));
+		rowY += rowInc;
+		addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col1, rowY)), module, Loopy::LOOP_START_PARAM));
+		addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col2, rowY)), module, Loopy::LOOP_LENGTH_PARAM));
 		// addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(col3, rowY)), module, Loopy::IMPUSE_LPF_PARAM));
 
 		// rowY += rowInc;
