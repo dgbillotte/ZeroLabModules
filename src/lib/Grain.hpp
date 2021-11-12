ea13c4fb7c9612b7650e5f530faa7378a0d80c62
Daniel Billotte <daniel@litfuze.com>
2021-11-12 01:36:47 -0700

    couple little clean ups

--- src/lib/Grain.hpp
+++ src/lib/Grain.hpp
@@ -6,7 +6,6 @@
 
 class Grain {
     const int COS_LUT_SIZE;
-    // const int COS_RAMP_SIZE;
     LUTPtr _cosLUT;
 
     int _length;
@@ -21,9 +20,6 @@ class Grain {
     float _lastEnv = 0;
     float _lastWav = 0;
 
-    // LUT cosLUT = LUT(0.f, 2.f*M_PI, 1000, cos);
-
-    // static CosLUT cosLUT;
 
 public:
     void dump() {
@@ -33,12 +29,10 @@ public:
 
     Grain(float freq, int length, int sampleRate=44100, float envRampLengthPct=0.2f) :
         COS_LUT_SIZE(1024),
-        // COS_RAMP_SIZE(COS_LUT_SIZE/2),
         _cosLUT(ObjectStore::getStore()->loadLUT("COS_0_2PI_1024", 0.f, 2.f*M_PI, COS_LUT_SIZE, [](float x) { return (cos(x)+1.f)/2.f; })),
         _length(length),
         _phaseInc(2.f * M_PI * freq / sampleRate),
         _envRampLength(length * envRampLengthPct),
-        // _envInc(COS_RAMP_SIZE / _envRampLength),
         _envInc(M_PI / _envRampLength),
         _envRampTwo(length - _envRampLength - 1)
     {}
@@ -52,12 +46,9 @@ public:
 
     float nextSample() {
         if(_idx < _length) {
-            // grain is running
             _lastWav = _nextWaveSample();
             _lastEnv = _nextEnvelopeValue();
             return _lastWav * _lastEnv;
-        } else {
-            std::cout << "do we get here?" << std::endl;
         }
         return 0.f;
     }
@@ -87,7 +78,6 @@ protected:
             float theta = (_idx - _envRampTwo) * _envInc;
             out = _cosLUT->at(theta);
         }
-        // std::cout << "env: " << out << std::endl;
         _idx++;
         return out;
     }
